935c6a907dfa470e2c29df3cb7e437011d1ec626
Otávio Costa Dallo <ocostadallo@gmail.com>
2023-07-03 08:17:13 -0300

    feat: createUser test

--- src/user/tests/user.service.spec.ts
+++ src/user/tests/user.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { UserService } from '../user.service';
+import { PrismaService } from '../../prisma/prisma.service'
+
+type CreateUserInputDto = {
+  id: number;
+  name: string;
+  email: string;
+  cpf: string;
+  cnpj: string;
+  image: string;
+  balance: number;
+  password: string;
+  isAdmin: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, PrismaService],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  const createUserInput = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    cpf: '12345678900',
+    cnpj: '',
+    image: 'path/to/image',
+    password: 'password123',
+  };
+
+  describe('create', () => {
+    it('should create a user with the provided data', async () => {
+      const create = prismaService.user.create;
+      const createdUser = { id: 1, ...createUserInput };
+
+      jest
+        .spyOn(prismaService.user, 'create')
+        .mockResolvedValue(createdUser as CreateUserInputDto);
+
+      const result = await userService.create(createUserInput);
+
+      expect(prismaService.user.create).toHaveBeenCalledWith(
+        expect.objectContaining({ data: expect.objectContaining(createUserInput) })
+      );
+      expect(result).toEqual(createdUser);
+    });
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
